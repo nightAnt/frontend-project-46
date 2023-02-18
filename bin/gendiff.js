dc8d7c96a1016c7c35afa1f7d64f8fe3a95dc579
Artyom Volkov <artemvolk2099@yandex.ru>
2023-02-18 21:50:52 +0300

    Added gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+
+const program = new Command();
+
+program
+  .description('CLI to some JavaScript string utilities')
+  .version('0.8.0');
+
+program.parse();
